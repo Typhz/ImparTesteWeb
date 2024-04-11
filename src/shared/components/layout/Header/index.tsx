6027aceb6a3588c80ad9bd3b3e858266272cd292
Patrick Reis <patrickbernardor@gmail.com>
2024-04-10 23:01:36 -0300

    feat: create header component

---
+++ src/shared/components/layout/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+import Logo from '../../../../assets/logo-teste.svg';
+const Header = () => {
+  return (
+    <div className="impar-header__container">
+      <img src={Logo} />
+    </div>
+  );
+}
+
+export default Header;
