cad6ec526332b5ea713d26235ce0ffee07c63d98
Patrick Reis <patrickbernardor@gmail.com>
2024-04-11 00:31:45 -0300

    feat: add button file upload

--- src/shared/components/inputs/FileUpload/index.tsx
+++ src/shared/components/inputs/FileUpload/index.tsx
@@ -1,11 +1,23 @@
 import { TextFieldProps } from '../TextField';
 import './styles.scss';
+import {useState} from 'react';
 
 const FileUpload = ({ label, name, register, ...props }: TextFieldProps) => {
+  const [fileName, setFileName] = useState<string[]>();
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const names = Array.from(files).map(file => file.name);
+      setFileName(names);
+    }
+  }
+
   return (
     <div className="file-upload__container">
-      <label htmlFor={props.id}>{label}</label>
-      <input type="file" {...props} {...register(name)}/>
+      <label className="file-upload__label" htmlFor={props.id}>{fileName ? fileName : label}</label>
+      <input type="file" id="file-upload" {...props} {...register(name)} onInput={handleFileChange}/>
+      <label className="file-upload__button" htmlFor={props.id}>Escolher arquivo</label>
     </div>
   );
 }
